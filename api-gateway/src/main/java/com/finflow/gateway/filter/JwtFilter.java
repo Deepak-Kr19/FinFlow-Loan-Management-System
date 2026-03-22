3951b25cab2b7a2164799dc907177f67f5ca40c2
Deepak-Kr19 <deepakkr1753@gmail.com>
2026-03-23 03:23:17 +0530

    Updated the API-Gateway

---
+++ api-gateway/src/main/java/com/finflow/gateway/filter/JwtFilter.java
@@ -0,0 +1,48 @@
+package com.finflow.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.*;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtFilter implements GlobalFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        String path = exchange.getRequest().getURI().getPath();
+
+        // Allow login & signup
+        if (path.contains("/auth/login") || path.contains("/auth/signup")) {
+            return chain.filter(exchange);
+        }
+
+        String authHeader = exchange.getRequest()
+                .getHeaders()
+                .getFirst(HttpHeaders.AUTHORIZATION);
+
+        // Check header
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+
+        // Validate token
+        if (!jwtUtil.validateToken(token)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+}
---
+++ api-gateway/src/main/java/com/finflow/gateway/filter/JwtUtil.java
@@ -0,0 +1,27 @@
+package com.finflow.gateway.filter;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "finflowsecretkeyfinflowsecretkey";
+
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
